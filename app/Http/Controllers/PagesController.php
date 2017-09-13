8380a29b567f9914ffaa620d3e0ccd5dda9370dd
Quang Nguyen <nguyenquang30795@gmail.com>
2017-09-13 23:44:33 +0100

    Clean up PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,34 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
-use App\Sponsor;
 
 class PagesController extends Controller {
 
     public function getIndex() {
-        $posts = Post::orderBy('created_at', 'desc')->limit(5)->get();
-        $sponsors = Sponsor::all();
-        return view('pages.welcome')->withPosts($posts)->withSponsors($sponsors);
+        return view('pages.welcome');
     }
 
     public function getAbout() {
-        $first = 'Quang';
-        $last = 'Nguyen';
-
-        $fullname = $first . " " . $last;
-        
-        $email = 'qn14@ic.ac.uk';
-
-        $data = [];
-        $data['email'] = $email;
-        $data['fullname'] = $fullname;
-
-        return view('pages.about')->withData($data);
+        return view('pages.about');
     }
 
     public function getSponsors() {
-        $sponsors = Sponsor::all();
-        return view('pages.sponsorship')->withSponsors($sponsors);
+        return view('pages.sponsorship');
     }
 }
